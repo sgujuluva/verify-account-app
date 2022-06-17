d6464974c578f15bc69e55daf6c1b97bfdeba987
sgujuluva <sanghee04@gmail.com>
2022-06-17 15:19:07 +0200

    logic done

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+let inputs = document.querySelectorAll("input");
 
+inputs[0].focus();
+
+inputs.forEach((item, index) => {
+  
+  item.addEventListener("keydown", () => {
+    item.value = "";
+      if(index < inputs.length -1 ){
+        setTimeout(() => inputs[index + 1].focus(), 100);
+      }else{
+        setTimeout(() => inputs[0].focus(), 100);
+      }
+   
+  });
+});
