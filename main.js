37634d88b4c7ac39765cb15d618836f535cf40dd
sgujuluva <sanghee04@gmail.com>
2022-06-17 15:46:52 +0200

    backspace functionalityy added

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
-let inputs = document.querySelectorAll("input");
+ let inputs = document.querySelectorAll("input");
 
 inputs[0].focus();
 
-inputs.forEach((item, index) => {
+ inputs.forEach((item, index) => {
   
-  item.addEventListener("keydown", () => {
-    item.value = "";
+  item.addEventListener("keydown", (e) => {
+    item.value = "";    
       if(index < inputs.length -1 ){
-        setTimeout(() => inputs[index + 1].focus(), 100);
-      }else{
+          console.log("input", e.key)
+        if(e.key === "Backspace"){
+            setTimeout(() => inputs[index -1].focus(),100);
+        }else{
+            setTimeout(() => inputs[index + 1].focus(), 100);
+        }
+       
+      } else{
         setTimeout(() => inputs[0].focus(), 100);
       }
    
   });
-});
+}); 
+ 
+
+ /* const codes = document.querySelectorAll('.code')
+
+codes[0].focus()
+
+codes.forEach((code, idx) => {
+    code.addEventListener('keydown', (e) => {
+        if(e.key >= 0 && e.key <=9) {
+            codes[idx].value = ''
+            setTimeout(() => codes[idx + 1].focus(), 10)
+        } else if(e.key === 'Backspace') {
+            setTimeout(() => codes[idx - 1].focus(), 10)
+        }
+    })
+})  */
